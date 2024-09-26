f99112f44a0f64ecca6b9e4d85cecde2c99de6c2
pbishal <pbishal60@gmail.com>
2024-09-26 13:28:45 +0530

    3 browsers added in base class

--- src/test/java/scenario2Test/BaseClass.java
+++ src/test/java/scenario2Test/BaseClass.java
@@ -4,30 +4,51 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import scenario2Pages.AdminPage;
 import scenario2Pages.LoginPage;
 
 public class BaseClass {
-	public WebDriver driver;
+//	public WebDriver driver;
 	public LoginPage lp;
 	public AdminPage ap;
+	public WebDriver driver = null;
+	String browser = "edge";
 
 	@BeforeTest
 	public void setUp() {
-		driver = new ChromeDriver();
+//		driver = new ChromeDriver();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		
+//		// Maximize the window
+//		driver.manage().window().maximize();
+//
+//		lp = new LoginPage(driver);
+//		ap = new AdminPage(driver);
+		if(browser.equals("chrome")) {
+			driver = new ChromeDriver();
+			
+		}else if(browser.equals("firefox")) {
+			driver = new FirefoxDriver();
+			
+		}else if(browser.equals("edge")) {
+			driver = new EdgeDriver();
+			
+		}
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		
-		// Maximize the window
 		driver.manage().window().maximize();
-
+		
 		lp = new LoginPage(driver);
 		ap = new AdminPage(driver);
-
 	}
 
 	@BeforeClass
